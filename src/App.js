69795231718edcf1af6bebb8bf85d38a3eb7c2a3
Jagdish Choudhary <jagdishchoudhary8570@gmail.com>
2023-06-08 21:14:58 +0530

    created add goal card

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AddGoals from './components/AddGoals';
+
+function App() {
+  return (
+    <div className="App">
+      <AddGoals />
+    </div>
+  );
+}
+
+export default App;
